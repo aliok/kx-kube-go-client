b36089fa20e8c09a0c69155b08ef9c368cbb559f
Ali Ok <aliok@redhat.com>
2019-09-21 23:52:05 +0300

    Informers, shared informers

--- cmd/main.go
+++ cmd/main.go
@@ -8,14 +8,16 @@ package main
 import (
 	"flag"
 	"fmt"
-	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
-	"log"
 	"os"
 	"path/filepath"
+	"time"
 	//
 	// Uncomment to load all auth plugins
 	// _ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -78,27 +80,92 @@ func main() {
 
 	}
 
-	/// USE PLAIN WATCH
+	//#region USE PLAIN WATCH
+	// https://stackoverflow.com/questions/40975307/how-to-watch-events-on-a-kubernetes-service-using-its-go-client
+	//watch, err := clientset.CoreV1().Pods("").Watch(metav1.ListOptions{})
+	//
+	//if err != nil {
+	//	panic(err.Error())
+	//}
+	//
+	//for event := range watch.ResultChan() {
+	//	fmt.Printf("Event type: %v\n", event.Type)
+	//	fmt.Printf("Event: %v\n", event)
+	//	p, ok := event.Object.(*corev1.Pod)
+	//	if !ok {
+	//		log.Fatal("unexpected type")
+	//	}
+	//	fmt.Println(p.Status.ContainerStatuses)
+	//	fmt.Println(p.Status.Phase)
+	//}
+	//#endregion
 
-	watch, err := clientset.CoreV1().Pods("").Watch(metav1.ListOptions{})
+	//#region USE INFORMER
+	// https://rancher.com/using-kubernetes-api-go-kubecon-2017-session-recap/
+	// https://stackoverflow.com/questions/40975307/how-to-watch-events-on-a-kubernetes-service-using-its-go-client
+	//watchList := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), string(v1.ResourcePods), v1.NamespaceAll, fields.Everything())
+	//
+	//// var store = cache.NewStore(nil)
+	//
+	//_, controller := cache.NewInformer(watchList, &v1.Pod{}, time.Second*30, cache.ResourceEventHandlerFuncs{
+	//	AddFunc: func(obj interface{}) {
+	//		fmt.Printf("pod added: %s \n", obj)
+	//	},
+	//	DeleteFunc: func(obj interface{}) {
+	//		fmt.Printf("pod deleted: %s \n", obj)
+	//
+	//		// FOLLOWING DOESN'T WORK
+	//		//item, exists, err := store.GetByKey("etcd-minikube")
+	//		//if err != nil {
+	//		//	fmt.Println("Error trying to get etcd pod from the watch store(cache)")
+	//		//} else if !exists {
+	//		//	fmt.Println("Cannot find etcd pod. Strange!")
+	//		//} else {
+	//		//	fmt.Printf("Etcd pod: %v", item)
+	//		//}
+	//	},
+	//	UpdateFunc: func(oldObj, newObj interface{}) {
+	//		fmt.Printf("pod updated: %s \n", oldObj)
+	//	},
+	//})
+	//
+	//stop := make(chan struct{})
+	//defer close(stop)
+	//go controller.Run(stop)
+	//
+	//for {
+	//	time.Sleep(time.Second)
+	//}
+	//#endregion
 
-	if err != nil {
-		panic(err.Error())
+	// region USE SHARED INFORMER
+	// https://rancher.com/using-kubernetes-api-go-kubecon-2017-session-recap/
+	// https://stackoverflow.com/questions/40975307/how-to-watch-events-on-a-kubernetes-service-using-its-go-client
+
+	watchList := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), string(v1.ResourcePods), v1.NamespaceAll, fields.Everything())
+
+	informer := cache.NewSharedInformer(watchList, &v1.Pod{}, time.Second*30)
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			fmt.Printf("pod added: %s \n", obj)
+		},
+		DeleteFunc: func(obj interface{}) {
+			fmt.Printf("pod deleted: %s \n", obj)
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			fmt.Printf("pod updated: %s \n", oldObj)
+		},
+	})
+
+	stop := make(chan struct{})
+	defer close(stop)
+	go informer.Run(stop)
+
+	for {
+		time.Sleep(time.Second)
 	}
 
-	for event := range watch.ResultChan() {
-		fmt.Printf("Event type: %v\n", event.Type)
-		fmt.Printf("Event: %v\n", event)
-		p, ok := event.Object.(*corev1.Pod)
-		if !ok {
-			log.Fatal("unexpected type")
-		}
-		fmt.Println(p.Status.ContainerStatuses)
-		fmt.Println(p.Status.Phase)
-	}
-
-	// USE INFORMER
-	// TODO:
+	// endregion
 
 }
 
